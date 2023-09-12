91224b7797c1c7de286b72737c816f8c8219c967
viniciusaznar <viniciusaznar@gmail.com>
2023-09-12 16:19:42 -0300

    Banco de dados H2 e criacão automática da base de dados

--- src/main/java/com/vinicius/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/vinicius/cursomc/resources/CategoriaResource.java
@@ -12,17 +12,17 @@ import com.vinicius.cursomc.domain.Categoria;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Categoria> listar() {
-		
+
 		Categoria cat1 = new Categoria(1, "Informática");
 		Categoria cat2 = new Categoria(2, "Escritóario");
-		
+
 		List<Categoria> lista = new ArrayList<>();
 		lista.add(cat1);
 		lista.add(cat2);
-		
-		return lista; 
+
+		return lista;
 	}
 }
