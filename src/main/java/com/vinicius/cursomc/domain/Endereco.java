5cb3e519c504e473d3ea70557f7b4319a096bf59
viniciusaznar <viniciusaznar@gmail.com>
2023-09-15 01:06:24 -0300

    Endpoint /clientes/{id] disponível

--- src/main/java/com/vinicius/cursomc/domain/Endereco.java
+++ src/main/java/com/vinicius/cursomc/domain/Endereco.java
@@ -3,6 +3,8 @@ package com.vinicius.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class Endereco implements Serializable {
 	private String bairro;
 	private String cep;
 	
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
